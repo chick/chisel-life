f94aec4f5d18dbe250562382b14733687509080f
chick <chick@qrhino.com>
2018-06-11 10:53:26 -0700

    Restructuring to chisel3

--- src/main/scala/life/LifeCell.scala
+++ src/main/scala/life/LifeCell.scala
@@ -1,42 +1,46 @@
+// See README.md for license details.
+
 package life
 
-import Chisel._
+import chisel3._
+import chisel3.iotesters.PeekPokeTester
+import chisel3.util.MuxCase
 
 /**
- * Created by chick on 11/9/15.
- * Implements a single cell of a conway life space
- */
+  * Created by chick on 11/9/15.
+  * Implements a single cell of a conway life space
+  */
 class LifeCell extends Module {
-  val io = new Bundle {
-    val running     = Bool(INPUT)
-    val top_left    = UInt(INPUT, width=4)
-    val top_center  = UInt(INPUT, width=4)
-    val top_right   = UInt(INPUT, width=4)
-    val mid_left    = UInt(INPUT, width=4)
-    val mid_right   = UInt(INPUT, width=4)
-    val bot_left    = UInt(INPUT, width=4)
-    val bot_center  = UInt(INPUT, width=4)
-    val bot_right   = UInt(INPUT, width=4)
+  val io = IO(new Bundle {
+    val running     = Input(Bool())
+    val top_left    = Input(UInt(4.W))
+    val top_center  = Input(UInt(4.W))
+    val top_right   = Input(UInt(4.W))
+    val mid_left    = Input(UInt(4.W))
+    val mid_right   = Input(UInt(4.W))
+    val bot_left    = Input(UInt(4.W))
+    val bot_center  = Input(UInt(4.W))
+    val bot_right   = Input(UInt(4.W))
 
-    val set_alive   = Bool(INPUT)
-    val set_dead    = Bool(INPUT)
+    val set_alive   = Input(Bool())
+    val set_dead    = Input(Bool())
 
-    val is_alive    = Bool(OUTPUT)
-  }
+    val is_alive    = Output(Bool())
+  })
 
   def set_neighbor(neighbor: LifeCell, delta_row:Int, delta_col: Int) {
     if(delta_row < 0 && delta_col < 0)   io.bot_left   := neighbor.io.is_alive
     if(delta_row < 0 && delta_col == 0)  io.bot_center := neighbor.io.is_alive
     if(delta_row < 0 && delta_col > 0 )  io.bot_right  := neighbor.io.is_alive
     if(delta_row == 0 && delta_col < 0)  io.mid_left   := neighbor.io.is_alive
-    if(delta_row == 0 && delta_col == 0) throwException("bad connection")
+    if(delta_row == 0 && delta_col == 0) throw new Exception("bad connection")
     if(delta_row == 0 && delta_col > 0)  io.mid_right  := neighbor.io.is_alive
     if(delta_row > 0 && delta_col < 0)   io.top_left   := neighbor.io.is_alive
     if(delta_row > 0 && delta_col == 0)  io.top_center := neighbor.io.is_alive
     if(delta_row > 0 && delta_col > 0)   io.top_right  := neighbor.io.is_alive
   }
 
-  val is_alive = Reg(init=Bool(false))
+  val is_alive = RegInit(false.B)
 
   val sum0 = io.top_left + io.top_center
   val sum1 = io.top_right + io.mid_left
@@ -50,40 +54,40 @@ class LifeCell extends Module {
 
   is_alive := MuxCase(is_alive,
     Seq(
-      io.set_alive              -> Bool(true),
-      io.set_dead               -> Bool(false),
-      (io.running && is_alive)  -> (neighbor_sum === UInt(2) || neighbor_sum === UInt(3)),
-      (io.running && !is_alive) -> (neighbor_sum === UInt(3))
+      io.set_alive              -> true.B,
+      io.set_dead               -> false.B,
+      (io.running && is_alive)  -> (neighbor_sum === 2.U || neighbor_sum === 3.U),
+      (io.running && !is_alive) -> (neighbor_sum === 3.U)
     )
   )
-//  when(io.set_alive) {
-//    is_alive := Bool(true)
-//  }.elsewhen(io.set_dead) {
-//    is_alive := Bool(false)
-//  }
-//
-//  when(io.running) {
-//    when(is_alive) {
-//      is_alive := neighbor_sum === UInt(2) || neighbor_sum === UInt(3)
-//    } otherwise {
-//      is_alive := neighbor_sum === UInt(3)
-//    }
-//  }
+  //  when(io.set_alive) {
+  //    is_alive := Bool(true)
+  //  }.elsewhen(io.set_dead) {
+  //    is_alive := Bool(false)
+  //  }
+  //
+  //  when(io.running) {
+  //    when(is_alive) {
+  //      is_alive := neighbor_sum === UInt(2) || neighbor_sum === UInt(3)
+  //    } otherwise {
+  //      is_alive := neighbor_sum === UInt(3)
+  //    }
+  //  }
 
   io.is_alive := is_alive
 }
 
-class LifeCellTests(c: LifeCell) extends Tester(c) { self =>
+class LifeCellTests(c: LifeCell) extends PeekPokeTester(c) { self =>
   def set_neighbors(
-                     ntl: Int, ntc: Int, ntr: Int,
-                     nml: Int, nmc: Int, nmr: Int,
-                     nbl: Int, nbc: Int, nbr: Int): Unit = {
+    ntl: Int, ntc: Int, ntr: Int,
+    nml: Int, nmc: Int, nmr: Int,
+    nbl: Int, nbc: Int, nbr: Int): Unit = {
     poke(c.io.top_left, ntl)
     poke(c.io.top_center, ntc)
     poke(c.io.top_right, ntr)
     poke(c.io.mid_left, nml)
     // center "neighbor" is the value of the cell itself
-    poke(c.is_alive, nmc)
+    // poke(c.is_alive, nmc)
     poke(c.io.mid_right, nmr)
     poke(c.io.bot_left, nbl)
     poke(c.io.bot_center, nbc)
@@ -196,10 +200,10 @@ class LifeCellTests(c: LifeCell) extends Tester(c) { self =>
 
 object LifeCell {
   def main(args: Array[String]): Unit = {
-    chiselMainTest(
-      Array[String]("--backend", "c", "--compile", "--test", "--genHarness"),
-//      Array[String]("--backend", "dot"),
-      () => Module(new LifeCell())
+    iotesters.Driver.execute(
+      Array[String]("--backend-name", "firrtl"),
+      //      Array[String]("--backend", "dot"),
+      () => new LifeCell()
     ) {
       c => new LifeCellTests(c)
     }
